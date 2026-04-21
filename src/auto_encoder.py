d5126bd81c5a0b9daaffc62aa01e8b7f20b22142
Cameron L. Nolen <169953917+BigKingCam@users.noreply.github.com>
2026-04-21 15:21:07 -0700

    path vars to train, val, test images

--- src/auto_encoder.py
+++ src/auto_encoder.py
@@ -13,15 +13,15 @@ from pathlib import Path
 # from dataset import Dataset
 # from noise import add_noise
 
-from .scripts.download_dataset import get_cbsd68_path
-from .scripts.download_dataset import get_bsds500_path
+from download_dataset import get_cbsd68_path
+from download_dataset import get_bsds500_path
 
 BASE_DIR: Path = Path(__file__).resolve().parents[2]
 
-cbsd68_img_folder: Path = get_cbsd68_path()
+cbsd68_img_folder: Path = get_cbsd68_path()  # CBSD68/
 cbsd_ground_truth: Path = cbsd68_img_folder / "original_png"
 
-bsds500_img_folder: Path = get_bsds500_path()
+bsds500_img_folder: Path = get_bsds500_path()  # BSDS500/
 bsd500_train: Path = bsds500_img_folder / "train"
 bsd500_val: Path = bsds500_img_folder / "val"
 bsd500_test: Path = bsds500_img_folder / "test"
@@ -44,6 +44,14 @@ def build_image_set(folder: Path) -> list[str]:
     return image_paths
 
 
+# def load_datasets():
+#     train_imgs: list[str] = build_image_set(bsd500_train)
+#     val_imgs: list[str] = build_image_set(bsd500_val)
+#     test_imgs: list[str] = build_image_set(bsd500_test)
+#     return train_imgs, val_imgs, test_imgs
+
+
 if __name__ == "__main__":
-    training_imgs: list[str] = build_image_set(bsd500_train)
-    validation_imgs: list[str] = build_image_set(bsd500_val)
+    train_imgs: list[str] = build_image_set(bsd500_train)
+    val_imgs: list[str] = build_image_set(bsd500_val)
+    test_imgs: list[str] = build_image_set(bsd500_test)
